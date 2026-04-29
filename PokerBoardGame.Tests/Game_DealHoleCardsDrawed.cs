d37387e80ec8fa45356e4ef3401c47b9ba974fe9
anuardp <nuradibphasya@gmail.com>
2026-04-29 14:56:03 +0700

    adding comments related to file directory

--- PokerBoardGame.Tests/Game_DealHoleCardsDrawed.cs
+++ PokerBoardGame.Tests/Game_DealHoleCardsDrawed.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 
 [TestFixture]
-public class Game_DealHoleCardsDrawed
+public class Game_DealHoleCardsDrawed //GameTests
 {
     private Game _game;
     [SetUp]
